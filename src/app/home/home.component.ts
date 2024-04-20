f59fc586b37910038efbff9efd9e1c32c422abfa
elainecarminhola <elainecgcarminhola@gamil.com>
2024-04-20 18:15:38 -0300

    pagina home em construção

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,4 +12,6 @@ import { NgOptimizedImage } from '@angular/common';
 })
 export class HomeComponent {
 
+  public textSintese: string = "Somos uma organização não governamental dedicada a reunir mães de crianças autistas, fornecendo orientação, apoio e assistência em seus desafios diários. Nosso objetivo é capacitar e educar essas mães, fornecendo-lhes conhecimentos e recursos necessários para entender e lidar com o autismo de seus filhos. Além disso, organizamos eventos e atividades especialmente voltados para crianças autistas, promovendo seu bem-estar e inclusão na sociedade. Juntos, estamos construindo uma comunidade solidária e capacitada, onde todas as crianças, independentemente de suas necessidades especiais, possam florescer e alcançar seu pleno potencial."
+
 }
